49670fd40010ddcc89ad6d7a5843b5257b721530
ArseniZar <arseni.zarkiewicz@gmail.com>
2025-11-03 23:40:34 +0100

    Squashed commit of the following: fix/automation -> main
    commit 716c6ec58ab938a13d0c11d6e0266e2aaedce713
    Author: ArseniZar <arseni.zarkiewicz@gmail.com>
    Date:   Mon Nov 3 23:38:32 2025 +0100
    
        fix(automation): remove automated Docker container build

--- espWeb/automation/run.sh
+++ espWeb/automation/run.sh
@@ -2,7 +2,7 @@
 
 NAME="espweb"
 REPO_ROOT=$(git rev-parse --show-toplevel)
-FRONTEND_DIR="$REPO_ROOT/espWeb/frontend"
+# FRONTEND_DIR="$REPO_ROOT/espWeb/frontend"
 BACKEND_DIR="$REPO_ROOT/espWeb/backend"
 AUTOMATION_DIR="$REPO_ROOT/espWeb/automation"
 
@@ -10,7 +10,7 @@ DOCKERFILE_DIR1="/include"
 DOCKERFILE_DIR2="/automation"
 
 
-docker build -t "$NAME" -f "$AUTOMATION_DIR/Dockerfile" "$FRONTEND_DIR"
+# docker build -t "$NAME" -f "$AUTOMATION_DIR/Dockerfile" "$FRONTEND_DIR"
 
 docker run --rm \
   -v "$BACKEND_DIR/include/:$DOCKERFILE_DIR1" \
